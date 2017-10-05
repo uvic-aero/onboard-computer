b16d8990a043aa80cab173a97a6f8a37f5f41a56
Chris Hampu <chrishampu@outlook.com>
2017-10-04 17:58:33 -0700

    Add skeleton for http client/server

---
+++ api.py
@@ -0,0 +1,10 @@
+
+# An HTTP server that will receive commands from the GroundStation
+class API:
+    
+    def __init__(self):
+        pass
+
+    # Start the HTTP server
+    def start(self):
+        pass
