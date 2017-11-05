e36ed78fc4e8119cfd062560e5f82a894bf0cf91
TaeHun Kang <kangtae27@gmail.com>
2017-11-04 17:55:41 -0800

    Implemented Zoom in/out on api

--- api.py
+++ api.py
@@ -1,15 +1,15 @@
 import tornado.ioloop
 import tornado.web
 from camera import camera
+
 # An HTTP server that will receive commands from the GroundStation
 class API:
-    
+
 	def __init__(self):
 		self.routes = [(r"/", MainHandler),
 		(r"/zoom/in", ZoomIn),
 		(r"/zoom/out", ZoomOut),
-		(r"/STILL", TakePicture)]
-		
+		(r"/still", TakePicture)]		
 
     # Start the HTTP server
 	def start(self):
@@ -21,30 +21,36 @@ class API:
 		tornado.ioloop.IOLoop.current().stop()
 		
 		
-class MainHandler(tornado.web.RequestHandler): #All Functions in Class Camera
+class MainHandler(tornado.web.RequestHandler):
 	def get(self):
 		self.write("Main Page of Onboard Computer Web Server	")
 			
 class TakePicture(tornado.web.RequestHandler):
 	def get(self):
 		self.write("Picture Page")
-		#TODO: Call Capture function on Camera
-		#take_picture(self)
+		camera.take_picture()
+		#TODO: Listed Below
+		#Return an HTTP 200 if the call succeeded 
+		#400-level code if the call failed with reason
+		#This call will depend on the camera being in Liveview mode to tell the camera to capture a still image
+		#will pause the liveview until the image is complete. This call may take many seconds to complete,
+		#but should not block the rest of the API***
+		
 
 class ZoomIn(tornado.web.RequestHandler):
 	def get(self):
 		self.write("Picture Page")
-		#TODO: Call Zoom In function on Camera
-		#zoom_in(self)
+		camera.zoom_in()
+		#TODO: Test for functionality
 
 class ZoomOut(tornado.web.RequestHandler):
 	def get(self):
 		self.write("Picture Page")
-		#TODO: Call Zoom out function on Camera
-		#zoom_out(self)		
+		camera.zoom_out()
+		#TODO: Test for functionality
 		
 
 #For Debugging Use (Running Independantly for now):
 if __name__ == "__main__":
 	server = API()
-	server.start()    
+	server.start()
