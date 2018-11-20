756b2a1b0d27e187ad8c90f5f4855e57b533d17e
Noah Clarke <noahwclarke@gmail.com>
2018-11-19 18:59:07 -0800

    Bug fixes to systemd service

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 if [ "$#" -ne 1 ]; then
     echo "Using current directory as install directory"
-    path = $(pwd)
-else; then
-    path = $1
+    path=$(pwd)
+else
+    path=$1
 fi
-sed -i -e "s/@dir/${path}/g" ./obc.service
-cp obc.service /etc/systemd/system
+sed -i -e "s@dir@${path}@g" ./obc.service
+sudo cp obc.service /etc/systemd/system
 sudo systemctl start obc.service
 sudo systemctl enable obc.service
