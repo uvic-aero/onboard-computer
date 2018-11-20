1505c3ae4a47eba53ed4008b3891cd0cd51fa281
Noah Clarke <noahwclarke@gmail.com>
2018-11-19 18:40:06 -0800

    Updated install script to allow a dynamic install location

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+if [ "$#" -ne 1 ]; then
+    echo "Using current directory as install directory"
+    path = $(pwd)
+else; then
+    path = $1
+fi
+sed -i -e "s/@dir/${path}/g" ./obc.service
 cp obc.service /etc/systemd/system
 sudo systemctl start obc.service
 sudo systemctl enable obc.service
