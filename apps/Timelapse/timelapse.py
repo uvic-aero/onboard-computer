558c20946e2a01293706d92c435a87012ef729b9
Dragon Prevost <dragon@dkp.io>
2018-12-03 16:41:08 -0800

    added import and start stop messages to timelapse

--- apps/Timelapse/timelapse.py
+++ apps/Timelapse/timelapse.py
@@ -1,6 +1,7 @@
 import asyncio
 from tornado import web
 import datetime
+from picamera import PiCamera
 
 class Timelapse:
     def __init__(self):
@@ -11,12 +12,14 @@ class Timelapse:
     # thinks that this class should not take the form of a process
 
     def start(self):
+        print('starting timelapse')
         #this function is responsible for inidtializing connections 
         #and processes that may be used by the ImageService class
         self.status = 'maybe running'
         pass
 
     def stop(self):
+        print('stopping timelapse')
         self.status = 'down'
         pass
 
