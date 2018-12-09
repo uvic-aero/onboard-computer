756a055174944753b5b7617064ad2e8920a78dbe
Dragon Prevost <dragon@dkp.io>
2018-12-09 13:19:39 -0800

    for timelapse - importing routes instead of handlers

--- apps/Routes/routes.py
+++ apps/Routes/routes.py
@@ -1,18 +1,6 @@
 #   import app GET responses
 from apps.ImageService.imageService import ImageServiceStatus, ImageServiceStart, ImageServiceStop
-from apps.Timelapse.timelapse import TimelapseStatus, TimelapseStart, TimelapseStop, TimelapseSetInterval
 from apps.VideoDisplay.videoDisplay import VideoDisplayStatus, VideoDisplayStart, VideoDisplayStop
+from apps.Timelapse.routes import routes as tlRoutes
 
-routes = [
-    (r"/status/imageService", ImageServiceStatus),
-    (r"/status/timelapse", TimelapseStatus),
-    (r"/status/videoDisplay", VideoDisplayStatus),
-    (r"/start/imageService", ImageServiceStart),
-    (r"/start/timelapse", TimelapseStart),
-    (r"/start/videoDisplay", VideoDisplayStart),
-    (r"/stop/imageService", ImageServiceStop),
-    (r"/stop/timelapse", TimelapseStop),
-    (r"/stop/videoDisplay", VideoDisplayStop),
-    (r"/setInterval/timelapse", TimelapseSetInterval),
-]
-
+routes = tlRoutes
