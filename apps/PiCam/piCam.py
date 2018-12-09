c89356767f3e89e9d9a3a80115c72ea670ff2fe7
Dragon Prevost <dragon@dkp.io>
2018-12-09 11:57:13 -0800

    commented picamera dependant lines

--- apps/PiCam/piCam.py
+++ apps/PiCam/piCam.py
@@ -1,26 +1,32 @@
-from picamera import PiCamera
+#from picamera import PiCamera
 import time
 
 class PiCam:
     def __init__(self):
-        self.camera = PiCamera()
-    
+        pass
+#       self.camera = PiCamera()
+#   
     def take_picture(self):
-        file = open('/home/pi/images/' + str(time.time())[:-8] + '.jpg', 'wb')
-        self.camera.capture(file)
-        file.close()
-    
+        pass
+#       file = open('/home/pi/images/' + str(time.time())[:-8] + '.jpg', 'wb')
+#       self.camera.capture(file)
+#       file.close()
+#   
     def start_video(self):
-        file = open('/home/pi/images/' + str(time.time())[:-8] + '.h264', 'wb')
-        self.camera.start_recording(file)
+        pass
+#       file = open('/home/pi/images/' + str(time.time())[:-8] + '.h264', 'wb')
+#       self.camera.start_recording(file)
     def stop_video(self, file):
-        self.camera.stop_recording()
-        file.close()
-    
+        pass
+#       self.camera.stop_recording()
+#       file.close()
+#   
     def start_preview(self):
-        self.camera.start_preview()
+        pass
+#       self.camera.start_preview()
     def stop_preview(self):
-        self.camera.stop_preview()
-
+        pass
+#       self.camera.stop_preview()
+#
 piCam = PiCam()
 
