5bca967dd26437ebe9067e4f53cf84dbfd1b4180
Dragon Prevost <dragon@dkp.io>
2018-12-09 16:49:45 -0800

    importing routes from each app

--- apps/Routes/routes.py
+++ apps/Routes/routes.py
@@ -1,7 +1,7 @@
-#   import app GET responses
+# import routes from each application
 from apps.Timelapse.routes import routes as tRoutes
 from apps.ImageService.routes import routes as isRoutes
 from apps.VideoDisplay.routes import routes as vdRoutes
 
-
+# Concatonate all available routes
 routes = tRoutes + isRoutes + vdRoutes
