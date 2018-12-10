e52132d9a8140017936fcb36970ea04bcba93d76
Dragon Prevost <dragon@dkp.io>
2018-12-09 16:56:38 -0800

    added video display routes to routes file

---
+++ apps/VideoDisplay/routes.py
@@ -0,0 +1,7 @@
+from apps.VideoDisplay.handlers import Status, Stop, Start 
+
+routes = [
+    (r"/status/videoDisplay", Status),
+    (r"/start/videoDisplay", Start),
+    (r"/stop/videoDisplay", Stop),
+]
