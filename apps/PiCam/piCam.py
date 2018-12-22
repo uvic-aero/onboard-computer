0aa4e58ecdacf350f7e062547968e04f8f4130ad
Dragon Prevost <dragon@dkp.io>
2018-12-21 17:22:32 -0800

    folder created only once

--- apps/PiCam/piCam.py
+++ apps/PiCam/piCam.py
@@ -7,16 +7,21 @@ class PiCam:
     def __init__(self):
         self.camera = PiCamera()
         self.now = datetime.datetime.now()
+        self.folder_not_created = True
    
     def take_picture(self):
         date = str(self.now)[:10]
         path = '/home/pi/images/'+ date 
-        try:
-            os.mkdir(path)
-        except OSError:
-            print ("Creation of the directory %s failed" % path)
-        else:
-            print ("Successfully created the directory %s " % path)
+        if self.folder_not_created:
+            try:
+                os.mkdir(path)
+            except OSError:
+                print ("Creation of the directory %s failed" % path)
+            else:
+                print ("Successfully created the directory %s " % path)
+
+            self.folder_not_created = False
+
         file = open('/home/pi/images/'+ date +'/' + str(time.time())[:-8] + '.jpg', 'wb')
         self.camera.capture(file)
         file.close()
