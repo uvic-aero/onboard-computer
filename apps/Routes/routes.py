a8020f5a446080a240910b2371d90629e1bdc02c
Dragon Prevost <dragon@dkp.io>
2019-01-12 17:14:29 -0800

    added new piCam routes to application routes

--- apps/Routes/routes.py
+++ apps/Routes/routes.py
@@ -2,6 +2,7 @@
 from apps.Timelapse.routes import routes as tRoutes
 from apps.ImageService.routes import routes as isRoutes
 from apps.VideoDisplay.routes import routes as vdRoutes
+from apps.PiCam.routes import routes as pcRoutes
 
 # Concatonate all available routes
-routes = tRoutes + isRoutes + vdRoutes
+routes = tRoutes + isRoutes + vdRoutes + pcRoutes
