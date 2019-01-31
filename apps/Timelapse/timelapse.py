eadf06bdb32ea1c6ef00d532f0f2f3b0badf66f6
Peter Van Gorp <p.vangorp95@gmail.com>
2019-01-30 18:48:39 -0800

    added ability to set a temporary interval in timelapse

--- apps/Timelapse/timelapse.py
+++ apps/Timelapse/timelapse.py
@@ -6,10 +6,14 @@ from apps.PiCam.piCam import piCam
 
 class Timelapse:
     interval = 3 #set default interval length.
+    prevInterval = 3
+    duration = 0
+    photo_count = -1
 
     def __init__(self):
         self.status = 'Down'
         self.loop_flag = True
+        self.stop_burst = False
         
     def start(self):
         self.loop_flag = True
@@ -26,9 +30,17 @@ class Timelapse:
         self.loop_flag = False
 
     def start_timelapse(self):
+        self.photo_count = -1
         while self.loop_flag:
             
-            time.sleep(self.interval) # Sleep for 3 seconds
+            time.sleep(float(self.interval)) # Sleep for 3 seconds
+            
+            if self.photo_count > 0:
+                self.photo_count = self.photo_count - 1 
+            if self.photo_count == 0:
+                self.photo_count = self.photo_count - 1
+                self.interval = self.prevInterval
+                self.duration = 0
             # This loop is used to trigger a photo every X seconds, using piCam.take_picture().
             # My recomendation is to take a photo then use the 
             # time.sleep(seconds) function to pause the loop for a desired amount of time.
@@ -42,6 +54,20 @@ class Timelapse:
             self.interval = newInterval
         except:
             print("invalid interval entered\n")
+
+        pass
+
+    def set_duration(self, newDuration, burstInterval):
+        # take duration and new burst interval. for duration, take photos at burst interval.
+        # return to previous interval after duration. include option to exit burst at any time. 
+	# move variables to main class, count to current delay. new function to get interval + handlers
+        try:
+            self.duration = newDuration
+            self.prevInterval = self.interval
+            self.interval = burstInterval
+            self.photo_count = float(self.duration) / float(self.interval)
+        except:
+            print("could not set new duration\n")
 
         pass
 
