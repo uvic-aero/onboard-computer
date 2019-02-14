b050a47f1127c4cab9fad6c8e6c6d011b9792db8
nfrancis <nfrancis@uvic.ca>
2019-02-13 19:09:54 -0800

    Created backend functions, handlers and routes to change camera settings for picam app

--- apps/PiCam/handlers.py
+++ apps/PiCam/handlers.py
@@ -1,6 +1,7 @@
 from tornado import web
 from apps.PiCam.piCam import piCam 
 
+#All other handlers are a copy of status handler. Change others, but not this one.
 class Status(web.RequestHandler):
     def set_default_headers(self):
         self.set_header("Access-Control-Allow-Origin", "*")
@@ -20,4 +21,498 @@ class Status(web.RequestHandler):
     def options(self):
         self.set_status(204)
         self.finish()
+
+class TakePicture(web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+
+    @web.asynchronous
+    def get(self):
+        piCam.take_picture()
+        try:
+            self.write({
+                'service':'PiCam',
+                'status':piCam.status})
+            self.finish()
+        except:
+            print('Error Writing Request Response')
+    @web.asynchronous 
+    def options(self):
+        self.set_status(204)
+        self.finish()
+
+class StartVideo(web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+
+    @web.asynchronous
+    def get(self):
+        piCam.start_video()
+        try:
+            self.write({
+                'service':'PiCam',
+                'status':piCam.status})
+            self.finish()
+        except:
+            print('Error Writing Request Response')
+    @web.asynchronous 
+    def options(self):
+        self.set_status(204)
+        self.finish()
+
+class StopVideo(web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+
+    @web.asynchronous
+    def get(self):
+        piCam.stop_video()
+        try:
+            self.write({
+                'service':'PiCam',
+                'status':piCam.status})
+            self.finish()
+        except:
+            print('Error Writing Request Response')
+    @web.asynchronous 
+    def options(self):
+        self.set_status(204)
+        self.finish()
+
+class StartPreview(web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+
+    @web.asynchronous
+    def get(self):
+        piCam.start_preview()
+        try:
+            self.write({
+                'service':'PiCam',
+                'status':piCam.status})
+            self.finish()
+        except:
+            print('Error Writing Request Response')
+    @web.asynchronous 
+    def options(self):
+        self.set_status(204)
+        self.finish()
+
+class StopPreview(web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+
+    @web.asynchronous
+    def get(self):
+        piCam.stop_preview()
+        try:
+            self.write({
+                'service':'PiCam',
+                'status':piCam.status})
+            self.finish()
+        except:
+            print('Error Writing Request Response')
+    @web.asynchronous 
+    def options(self):
+        self.set_status(204)
+        self.finish()
+        
+class GetExposureCompensation(web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+
+    @web.asynchronous
+    def get(self):
+        piCam.get_exposure_compensation()
+        try:
+            self.write({
+                'service':'PiCam',
+                'status':piCam.status})
+            self.finish()
+        except:
+            print('Error Writing Request Response')
+    @web.asynchronous 
+    def options(self):
+        self.set_status(204)
+        self.finish()
+
+class SetExposureCompensation(web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+
+    @web.asynchronous
+    def get(self):
+        val = self.get_argument('val')
+        piCam.set_exposure_compensation(val)
+        print(val)
+        try:
+            self.write({
+                'service':'PiCam',
+                'status':piCam.status})
+            self.finish()
+        except:
+            print('Error Writing Request Response')
+    @web.asynchronous 
+    def options(self):
+        self.set_status(204)
+        self.finish()
+
+class IncExposureCompensation(web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+
+    @web.asynchronous
+    def get(self):
+        piCam.inc_exposure_compensation(val)
+        print(val)
+        try:
+            self.write({
+                'service':'PiCam',
+                'status':piCam.status})
+            self.finish()
+        except:
+            print('Error Writing Request Response')
+    @web.asynchronous 
+    def options(self):
+        self.set_status(204)
+        self.finish()
+
+class DecExposureCompensation(web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+
+    @web.asynchronous
+    def get(self):
+        piCam.dec_exposure_compensation()
+        print(val)
+        try:
+            self.write({
+                'service':'PiCam',
+                'status':piCam.status})
+            self.finish()
+        except:
+            print('Error Writing Request Response')
+    @web.asynchronous 
+    def options(self):
+        self.set_status(204)
+        self.finish()
+
+class GetShutterSpeed(web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+
+    @web.asynchronous
+    def get(self):
+        piCam.get_shutter_speed()
+        try:
+            self.write({
+                'service':'PiCam',
+                'status':piCam.status})
+            self.finish()
+        except:
+            print('Error Writing Request Response')
+    @web.asynchronous 
+    def options(self):
+        self.set_status(204)
+        self.finish()
+
+class SetShutterSpeed(web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+
+    @web.asynchronous
+    def get(self):
+        val = self.get_argument('val')
+        piCam.set_shutter_speed(val)
+        try:
+            self.write({
+                'service':'PiCam',
+                'status':piCam.status})
+            self.finish()
+        except:
+            print('Error Writing Request Response')
+    @web.asynchronous 
+    def options(self):
+        self.set_status(204)
+        self.finish()
+
+class IncShutterSpeed(web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+
+    @web.asynchronous
+    def get(self):
+        piCam.inc_shutter_speed()
+        try:
+            self.write({
+                'service':'PiCam',
+                'status':piCam.status})
+            self.finish()
+        except:
+            print('Error Writing Request Response')
+    @web.asynchronous 
+    def options(self):
+        self.set_status(204)
+        self.finish()
+
+class DecShutterSpeed(web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+
+    @web.asynchronous
+    def get(self):
+        piCam.dec_shutter_speed()
+        try:
+            self.write({
+                'service':'PiCam',
+                'status':piCam.status})
+            self.finish()
+        except:
+            print('Error Writing Request Response')
+    @web.asynchronous 
+    def options(self):
+        self.set_status(204)
+        self.finish()
+
+class GetAwbMode(web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+
+    @web.asynchronous
+    def get(self):
+        piCam.get_awb_mode()
+        try:
+            self.write({
+                'service':'PiCam',
+                'status':piCam.status})
+            self.finish()
+        except:
+            print('Error Writing Request Response')
+    @web.asynchronous 
+    def options(self):
+        self.set_status(204)
+        self.finish()
+
+
+class SetAwbMode(web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+
+    @web.asynchronous
+    def get(self):
+        val = self.get_argument('val')
+        piCam.set_awb_mode(val)
+        try:
+            self.write({
+                'service':'PiCam',
+                'status':piCam.status})
+            self.finish()
+        except:
+            print('Error Writing Request Response')
+    @web.asynchronous 
+    def options(self):
+        self.set_status(204)
+        self.finish()
+
+class GetAwbGains(web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+
+    @web.asynchronous
+    def get(self):
+        piCam.get_awb_gains()
+        try:
+            self.write({
+                'service':'PiCam',
+                'status':piCam.status})
+            self.finish()
+        except:
+            print('Error Writing Request Response')
+    @web.asynchronous 
+    def options(self):
+        self.set_status(204)
+        self.finish()
+
+class SetAwbGains(web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+
+    @web.asynchronous
+    def get(self):
+        val = self.get_argument('val')
+        piCam.set_awb_gains(val)
+        try:
+            self.write({
+                'service':'PiCam',
+                'status':piCam.status})
+            self.finish()
+        except:
+            print('Error Writing Request Response')
+    @web.asynchronous 
+    def options(self):
+        self.set_status(204)
+        self.finish()
+
+class IncAwbGains(web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+
+    @web.asynchronous
+    def get(self):
+        piCam.inc_awb_gains()
+        try:
+            self.write({
+                'service':'PiCam',
+                'status':piCam.status})
+            self.finish()
+        except:
+            print('Error Writing Request Response')
+    @web.asynchronous 
+    def options(self):
+        self.set_status(204)
+        self.finish()
+
+class DecAwbGains(web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+
+    @web.asynchronous
+    def get(self):
+        piCam.dec_awb_gains()
+        try:
+            self.write({
+                'service':'PiCam',
+                'status':piCam.status})
+            self.finish()
+        except:
+            print('Error Writing Request Response')
+    @web.asynchronous 
+    def options(self):
+        self.set_status(204)
+        self.finish()
+
+class GetIso(web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+
+    @web.asynchronous
+    def get(self):
+        piCam.get_iso()
+        try:
+            self.write({
+                'service':'PiCam',
+                'status':piCam.status})
+            self.finish()
+        except:
+            print('Error Writing Request Response')
+    @web.asynchronous 
+    def options(self):
+        self.set_status(204)
+        self.finish()
+
+class SetIso(web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+
+    @web.asynchronous
+    def get(self):
+        val = self.get_argument('val')
+        piCam.set_iso(val)
+        try:
+            self.write({
+                'service':'PiCam',
+                'status':piCam.status})
+            self.finish()
+        except:
+            print('Error Writing Request Response')
+    @web.asynchronous 
+    def options(self):
+        self.set_status(204)
+        self.finish()
+
+class IncIso(web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+
+    @web.asynchronous
+    def get(self):
+        piCam.inc_iso()
+        try:
+            self.write({
+                'service':'PiCam',
+                'status':piCam.status})
+            self.finish()
+        except:
+            print('Error Writing Request Response')
+    @web.asynchronous 
+    def options(self):
+        self.set_status(204)
+        self.finish()
+
+class DecIso(web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+
+    @web.asynchronous
+    def get(self):
+
+        piCam.dec_iso()
+        try:
+            self.write({
+                'service':'PiCam',
+                'status':piCam.status})
+            self.finish()
+        except:
+            print('Error Writing Request Response')
+    @web.asynchronous 
+    def options(self):
+        self.set_status(204)
+        self.finish()
+
 
--- apps/PiCam/routes.py
+++ apps/PiCam/routes.py
@@ -1,5 +1,28 @@
-from apps.PiCam.handlers import Status
+from apps.PiCam.handlers import Status, TakePicture, StartVideo, StopVideo, StartPreview, StopPreview, GetExposureCompensation, SetExposureCompensation, IncExposureCompensation, DecExposureCompensation, GetShutterSpeed, SetShutterSpeed, IncShutterSpeed, DecShutterSpeed, GetAwbMode, SetAwbMode, GetAwbGains, SetAwbGains, IncAwbGains, DecAwbGains, GetIso, SetIso, IncIso, DecIso 
 
 routes = [
     (r"/status/piCam", Status),
+    (r"/takePicture", TakePicture),
+    (r"/startVideo/piCam", StartVideo),
+    (r"/stopVideo/piCam", StopVideo),
+    (r"/startPreview/piCam", StartPreview),
+    (r"/stopPreview/piCam", StopPreview),
+    (r"/getExposureCompensation/piCam", GetExposureCompensation),
+    (r"/setExposureCompensation/piCam", SetExposureCompensation),
+    (r"/incExposureCompensation/piCam", IncExposureCompensation),
+    (r"/decExposureCompensation/piCam", DecExposureCompensation),
+    (r"/getShutterSpeed/piCam", GetShutterSpeed),
+    (r"/setShutterSpeed/piCam", SetShutterSpeed),
+    (r"/incShutterSpeed/piCam", IncShutterSpeed),
+    (r"/decShutterSpeed/piCam", DecShutterSpeed),
+    (r"/getAwbMode/piCam", GetAwbMode),
+    (r"/setAwbMode/piCam", SetAwbMode),
+    (r"/getAwbGains/piCam", GetAwbGains),
+    (r"/setAwbGains/piCam", SetAwbGains),
+    (r"/incAwbGains/piCam", IncAwbGains),
+    (r"/decAwbGains/piCam", DecAwbGains),
+    (r"/getIso/piCam", GetIso),
+    (r"/setIso/piCam", SetIso),
+    (r"/incIso/piCam", IncIso),
+    (r"/decIso/piCam", DecIso)
 ]
