c1b517b56ac42b064334ea9b375c9e39e6d203c3
dragonprevost <dragon@dkp.io>
2019-03-05 20:09:52 -0800

    removed comments and set a couple variables to follow the under score notation

--- apps/Timelapse/timelapse.py
+++ apps/Timelapse/timelapse.py
@@ -5,16 +5,19 @@ import _thread
 from apps.PiCam.piCam import piCam
 
 class Timelapse:
-    interval = 3 #set default interval length.
-    prevInterval = 3
-    duration = 0
-    photo_count = -1
 
     def __init__(self):
         self.status = 'Down'
         self.loop_flag = True
         self.stop_burst = False
-        
+
+        #set default interval length.
+        self.interval = 3 
+        self.prev_interval = 3
+        self.duration = 0
+        self.photo_count = -1
+
+    # Start the timelapse application     
     def start(self):
         self.loop_flag = True
         print('starting timelapse')
@@ -23,33 +26,31 @@ class Timelapse:
             _thread.start_new_thread( self.start_timelapse, ( ))
         except:
             print('Failed to Create Timelapse Thread')
-    
+
+    # Stop the timelapse application 
     def stop(self):
         print('stopping timelapse')
         self.status = 'Down'
         self.loop_flag = False
-
+        
+    # Run a loop that takes photos every X seconds    
     def start_timelapse(self):
         self.photo_count = -1
         while self.loop_flag:
-            
+
+            # Wait for a given interval between photo captures
             time.sleep(float(self.interval)) # Sleep for 3 seconds
             
+            # If Duration set, decrease photo count                 
             if self.photo_count > 0:
                 self.photo_count = self.photo_count - 1 
             if self.photo_count == 0:
                 self.photo_count = self.photo_count - 1
-                self.interval = self.prevInterval
+                self.interval = self.prev_interval
                 self.duration = 0
-            # This loop is used to trigger a photo every X seconds, using piCam.take_picture().
-            # My recomendation is to take a photo then use the 
-            # time.sleep(seconds) function to pause the loop for a desired amount of time.
-            pass
-    
+   
+    # Update the interval between photos being taken
     def set_interval(self, newInterval):
-        # This function is used to update the interval between each photo being taken.
-        # Hint, update this class to store a variable called interval, 
-        # then refer to that variable while inside the timelapse loop while sleeping.
         try:
             self.interval = newInterval
         except:
@@ -57,14 +58,13 @@ class Timelapse:
 
         pass
 
-    def set_duration(self, newDuration, burstInterval):
-        # take duration and new burst interval. for duration, take photos at burst interval.
-        # return to previous interval after duration. include option to exit burst at any time. 
-	# move variables to main class, count to current delay. new function to get interval + handlers
+    # For a given amount of time ( new_duration ), photos 
+    # will be captured with a temporary interval ( burst_interval )
+    def set_duration(self, new_duration, burst_interval):
         try:
-            self.duration = newDuration
-            self.prevInterval = self.interval
-            self.interval = burstInterval
+            self.duration = new_duration
+            self.prev_interval = self.interval
+            self.interval = burst_interval
             self.photo_count = float(self.duration) / float(self.interval)
         except:
             print("could not set new duration\n")
