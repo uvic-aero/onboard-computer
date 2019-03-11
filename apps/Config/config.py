9736def999b2a0a192008cb3569bca8cdf819e48
dragonprevost <dragon@dkp.io>
2019-03-10 19:19:50 -0700

    removed specific class variables in config

--- apps/Config/config.py
+++ apps/Config/config.py
@@ -4,9 +4,6 @@ class Config:
     
     def __init__(self):
         self.values = self.get_values()
-        self.groundstation_ip = self.values['groundstation']['ip'] 
-        self.groundstation_port = self.values['groundstation']['port']
-        self.obc_port = self.values['obc']['port']
         self.print_values()
 
     def get_values(self):
@@ -18,5 +15,5 @@ class Config:
         print('Configuration:' \
             '\n  OBC PORT: {0}'\
             '\n  GCS IP: {1}'\
-            '\n  GCS PORT: {2}'.format(self.obc_port, self.groundstation_ip, self.groundstation_port))
+            '\n  GCS PORT: {2}'.format(self.values['obc']['port'], self.values['groundstation']['ip'], self.values['groundstation']['port']))
 config = Config()
