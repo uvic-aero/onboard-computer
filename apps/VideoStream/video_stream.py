d5c9eabf0121b7900f79e468d105ce416ff01619
Joshua Newton <joshuacwnewton@gmail.com>
2019-10-02 11:29:15 -0700

    Created skeleton class file in new VideoStream app directory.

---
+++ apps/VideoStream/video_stream.py
@@ -0,0 +1,25 @@
+# Import Statements
+
+
+class VideoStream:
+    """Class for streaming video. """
+   # TODO: Write more complete docstring when distinction between classes is more clear.
+
+    def __init__(self):
+        self.status = (
+            "down"
+        )
+        pass
+
+    def start(self):
+        print("Starting VideoStream...")
+        self.status = "running"
+
+    def stop(self):
+        print("Stopping VideoStream...")
+        self.status = "down"
+
+    # TODO: Add skeletons for additional class methods when functionality of class is made more clear.
+    
+
+videoStream = VideoStream()
