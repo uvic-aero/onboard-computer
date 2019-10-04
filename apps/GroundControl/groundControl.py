7f4415a48752bfc2b02fd0461a355e438a37be93
SirDuck145 <matthiasharden@gmail.com>
2019-10-03 18:17:00 -0700

    Create a skeleton class for groundControl

---
+++ apps/GroundControl/groundControl.py
@@ -0,0 +1,33 @@
+#Imports needed
+
+class GroundControl():
+	#Used to control the ground vehicle
+
+	def __init__(self):
+		# TODO: Create and Calibrate Motor Controllers
+		self.frontLeft = 0
+		self.frontRight = 0
+		self.backLeft = 0
+		self.backRight = 0
+
+
+	# TODO: Create stepper/motor getters and setters
+	def setFrontLeft(value):
+		pass
+	def setFrontRight(value):
+		pass
+	def setBackLeft(value):
+		pass
+	def setBackRight(value):
+		pass
+
+	# Potentially not needed
+	def float getFrontLeft():
+		pass
+	def float getFrontRight():
+		pass
+	def float getBackLeft():
+		pass
+	def float getBackRight():
+		pass
+groundControl = GroundControl()
