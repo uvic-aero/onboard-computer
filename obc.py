acf16a89b73e9b7ce2ad946f675c5725039a0bc8
Logan Kuzyk <logan@kuzyk.net>
2019-10-14 18:59:47 -0700

    Fix obc.service.

--- obc.py
+++ obc.py
@@ -23,12 +23,8 @@ class OnboardComputer:
 
         # start apps
         imageService.start()
-<<<<<<< HEAD
         videoDisplay.start()
         pixhawk.start()
-=======
-        telemData.start()
->>>>>>> 4fc1f82be6b1373c222d146900e064766842638e
 
 
 
@@ -38,12 +34,8 @@ class OnboardComputer:
         # stop apps
         self.imageService.stop()
         self.timelapse.stop()
-<<<<<<< HEAD
         self.videoDisplay.stop()
         self.pixhawk()
-=======
-        self.telemData()
->>>>>>> 4fc1f82be6b1373c222d146900e064766842638e
 
 
 if __name__ == "__main__":
