e56efa4e44cfdc030c3c223b9d5ed73c52e9c482
= <=>
2019-11-08 10:53:34 -0800

    test video stream did during meeting on November 7th

--- apps/VideoStream/videoStream.py
+++ apps/VideoStream/videoStream.py
@@ -5,8 +5,10 @@ import cv2
 import math
 import threading
 import zlib
+import sys
 
 class VideoStream:
+
     """Class for streaming video. """
    # TODO: Write more complete docstring when distinction between classes is more clear.
 
@@ -14,16 +16,16 @@ class VideoStream:
         self.status = "down"
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.bind(('', 1201))
-        
+
         self.cap = cv2.VideoCapture(0)
-        
+
         #Configurations
         self.compression_level = 6 #default level of compression, we can turn it down for faster compression
-        self.port = 5100 
+        self.port = 5100
         self.img_length = 640 #New Resolution #Not used
         self.img_width = 640 #Not used
-        
-        
+
+
     def start(self):
         print("Starting VideoStream...")
         self.status = "running"
@@ -32,9 +34,13 @@ class VideoStream:
         print("Stopping VideoStream...")
         self.status = "down"
 
+    def send_frame(self,frame):
+        print(sys.getsizeof(frame))
+        self.socket.sendto(frame[:1300], ('0.0.0.0', 3444))
+
 
     # TODO: Add skeletons for additional class methods when functionality of class is made more clear.
-    def main_loop:
+    def main_loop(self):
         keep_running = 1
         while keep_running:
             data, address = sock.recvfrom(4)
@@ -48,22 +54,43 @@ class VideoStream:
                     sock.sendto("FAIL".encode('utf-8'), address)
                     continue
                 elif(data=="quit"):
-                    keep_running = False                
+                    keep_running = False
                 socket.sendto(buf, address)
         print("Quitting...")
         sock.close()
-                     
-    def buffer_frame(frame): 
+
+    def buffer_frame(frame):
         #TODO: frame is a numpy array?
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         #TODO: Review what resolution (size) we want the image, and create a configurable input
-        gray = cv2.resize(gray, (0, 0), fx=0.1, fy=0.1) 
+        gray = cv2.resize(gray, (0, 0), fx=0.1, fy=0.1)
         img = cv2.imencode('.jpg', gray)[1]
         buffer = img.toString()
         #compression
         buffer = zlib.compress(buffer, self.compression_level)
-        return buffer 
-        
-        
+        return buffer
 
-videoStream = VideoStream()        
+
+
+videoStream = VideoStream()
+
+#######put in file test.py 
+cap = cv2.VideoCapture(0)
+
+
+while True:
+
+    ret, frame = cap.read()
+    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY),1]
+    encimg = cv2.imencode('.jpg', grey, encode_param)[1].tostring()
+    print(sys.getsizeof(encimg))
+    #frame = cv2.imdecode(encimg,1)
+    videoStream.send_frame(encimg)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+videoStream.socket.close()
