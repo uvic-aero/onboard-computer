3f37707719ff21675bb76f1666473a659b502526
Logan Kuzyk <logan@kuzyk.net>
2019-11-21 20:34:48 -0800

    started on making listen function

--- apps/VideoStream/videoStream.py
+++ apps/VideoStream/videoStream.py
@@ -1,4 +1,3 @@
-# Import Statements
 import socket
 import time
 import zlib
@@ -34,17 +33,17 @@ class Connections:
             if(time.time() - self.times_since_heartbeat[i] > self.timeout):
                 self.remove(i)
             i = i - 1
+        return self.connections
 
 class VideoStream:
-
     """Class for streaming video. """
    # TODO: Write more complete docstring when distinction between classes is more clear.
 
     def __init__(self):
         self.status = "down"
+        self.port = 1201
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.socket.bind(('', 1201))
-        self.compression_level = 6 
+        self.connections = Connections(3)
 
     def start(self):
         print("Starting VideoStream...")
@@ -54,14 +53,29 @@ class VideoStream:
         print("Stopping VideoStream...")
         self.status = "down"
 
+    def send_frame(self, frame): #Do not use, will be replaced with listen()
+        data, address = self.socket.recvfrom(4)
+        data = data.decode('utf-8')
+
+        if (data == "get"):
+            self.socket.sendto(frame, address)
+
     def compress_frame(self, frame, quality):
         encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), quality]
         grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         encimg = cv2.imencode('.jpg', grey, encode_param)[1].tostring()
-        encimg = zlib.compress(encimg, 9)
-        
-        return encimg
-    def send_frame(self,frame):
-        self.socket.sendto(frame[:1600], ('0.0.0.0', 12345))
+        encimg = zlib.compress(encimg, -1)
+        videoStream.send_frame(encimg)
 
-videoStream = VideoStream() 
+    def listen(self, port=self.port):
+        self.socket.bind(('', port))
+        while True:
+            for connections in self.connections
+                data, address = self.socket.recvfrom(4)
+                if (data == "get"):
+                    
+
+    # TODO: Add skeletons for additional class methods when functionality of class is made more clear.
+    
+
+videoStream = VideoStream()
