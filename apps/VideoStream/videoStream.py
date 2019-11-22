0a11d2121fa9d1f4cab2e11f60cf44e726f5ac37
dragonprevost <dragon@dkp.io>
2019-11-21 18:44:52 -0800

    removed unused app state

--- apps/VideoStream/videoStream.py
+++ apps/VideoStream/videoStream.py
@@ -11,8 +11,7 @@ class VideoStream:
         self.status = "down"
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.bind(('', 1201))
-        self.compression_level = 6 #default level of compression, we can turn it down for faster compression
-        self.port = 5100
+        self.compression_level = 6 
 
     def start(self):
         print("Starting VideoStream...")
