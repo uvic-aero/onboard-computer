40f422be74bb0cb7b12fb572fcd1e263e4cc2aff
avesk <averykushner@gmail.com>
2019-12-28 15:43:21 -0800

    adds test files

---
+++ test/test_vision.py
@@ -0,0 +1,12 @@
+import unittest
+from apps.Vision import Vision
+
+
+class TestVision(unittest.TestCase):
+
+    def test_filer_contour(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
