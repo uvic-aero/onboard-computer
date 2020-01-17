c20f1380150f23169ea6eeafeacde0eb80a62ff2
Logan Kuzyk <logan@kuzyk.net>
2020-01-17 15:06:40 -0800

    No race conditions, fix more things in Connections class

--- apps/VideoStream/videoStream.py
+++ apps/VideoStream/videoStream.py
@@ -15,7 +15,8 @@ class Connections:
 
     def add(self, new_address):
         self.connections.append(new_address)
-        self.times_since_heartbeat.append(0)
+        self.times_since_heartbeat.append(time.time())
+        
 
     # Can remove by either address or index.
     def remove(self, to_remove):
@@ -23,19 +24,16 @@ class Connections:
             to_remove = self.connections.index(to_remove)
         del self.connections[to_remove]
         del self.times_since_heartbeat[to_remove]
-
-    def read_heartbeat(self, address):
+        
+    def read_heartbeat(self, address):     
         index = self.connections.index(address)
         self.times_since_heartbeat[index] = time.time()
 
-    # Clean up expired connections. Gets called everytime a new client connects, connections are cleaned.
-    def cleanup_connections(self):
-        i = 0
-        while i < len(self.times_since_heartbeat):
+    def cleanup(self):  
+        for i in range(len(self.times_since_heartbeat)):
             if(time.time() - self.times_since_heartbeat[i] > self.timeout):
+                print("Client", self.connections[i], "timed out.")
                 self.remove(i)
-            i = i + 1
-        return self.connections
 
     def update(self, address):
         if address in self.connections:
@@ -51,20 +49,20 @@ class VideoStream:
         self.status = "down"
         self.port = 1201
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.connections = Connections(4)
+        self.connections = Connections(2)
 
     def start(self):
         print("Starting VideoStream...")
         self.listen_thread()
         self.status = "running"
+        self.socket.settimeout(1)
 
     def stop(self):
         print("Stopping VideoStream...")
-        threading._shutdown()
+        self.socket.close()
         self.status = "down"
 
     def send_frame(self, frame, address, quality=4):
-        frame = encode_frame(frame)
         encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), quality]
         grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         frame = cv2.imencode('.jpg', grey, encode_param)[1].tostring()
@@ -75,31 +73,32 @@ class VideoStream:
         listen = threading.Thread(target = self.listen)
         listen.daemon = False
         listen.start()
-    
+
     def listen(self, port = None):
         if port is None:
             port = self.port
         self.socket.bind(('', port))
         print('Listening on port', port)
         while True:
-            self.connections.lock.acquire()
-            self.connections.cleanup_connections()
-            data, address = self.socket.recvfrom(3)
-            data = data.decode('utf-8')
-            if (data == "get"):
-                self.connections.update(address)
+            self.connections.cleanup()
             for address in self.connections.connections:
                 print(address)
-            self.connections.lock.release()
-            time.sleep(1)
+            try:    
+                data, address = self.socket.recvfrom(3)
+                data = data.decode('utf-8')
+                if (data == "get"):
+                    self.connections.lock.acquire()
+                    self.connections.update(address)
+                    for address in self.connections.connections:
+                        print(address)
+                    self.connections.lock.release()
+                continue
+            except socket.timeout:
+                continue
 
     def broadcast(self, frame):
-        self.connections.lock.acquire()
-        self.cleanup_connections()
         for address in self.connections.connections:
-            self.send_frame(frame, address)
-        self.connections.lock.release()
-            
+            self.send_frame(frame, address)            
 
     # TODO: Add skeletons for additional class methods when functionality of class is made more clear.
 
