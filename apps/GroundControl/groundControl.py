578d212492a1fd8ed30f8f250c85c9a023ececa1
= <=>
2020-01-16 19:07:26 -0800

    Testing on raspberry pi

--- apps/GroundControl/groundControl.py
+++ apps/GroundControl/groundControl.py
@@ -1,11 +1,55 @@
+
+import serial
+
 class GroundControl():
-	#Used to control the ground vehicle
+    #Used to control the ground vehicle
 
-	def __init__(self):
-		# TODO: Create and Calibrate Motor Controllers
-		self.front_left = 0
-		self.front_right = 0
-		self.back_left = 0
-		self.back_right = 0
+    def __init__(self):
 
-groundControl = GroundControl()
+
+        groundControl = GroundControl()
+
+
+        # open serial port
+    	#ser = serial.Serial('/dev/ttyUSB0')
+
+
+
+    def convert_to_binary(commands):
+    # Do something to convert the commands to binary
+    # Commands will be in a dictionary that looks like this
+
+    binary_string = "<"
+
+
+    if commands.get("180") == false:
+    binary_string.append('0 ')
+    else:
+    binary_string.append('1 ')
+
+    if commands.get("break") == false:
+    binary_string.append('0 ')
+    else:
+    binary_string.append('1 ')
+
+    #0 or 1 into string
+    binary_string.append(commands.get("dir1") + " ")
+
+    binary_string.append(commands.get("vel1") + " ")
+
+    binary_string.append(commands.get("dir2") + " ")
+
+    binary_string.append(commands.get("vel2") + " ")
+
+    binary_string.append(">")
+
+    return binary_string
+​
+
+    # not sure why 2 functions
+    def send_command_to_arduino(command):
+
+
+    return None
+
+    ser.close()
