9b99f151f2fd9fdbcb1f93846284bddbf2c03e7f
DerekSiemens <50340998+DerekSiemens@users.noreply.github.com>
2020-01-24 14:07:23 -0800

    Hashing out general functions and functionality

--- apps/Vision/DirectionVector.py
+++ apps/Vision/DirectionVector.py
@@ -1,18 +1,39 @@
+
+import numpy as np
+
 class DirectionVector:
 
    def __init__(self):
-      self.frames = []
-      self.probability_matrix = []
-      pass
+      self.frames_queue = queue.Queue(5)
+      self.shape = 2
+      self.heat_matrix = np.ones(self.shape,self.shape)
 
-   #Take in most recent frame and return the probability matrix
-   def compute_matrix(self, frame)
-         #Add frame to frames object
+   def get_oldest_frame(self):
+      oldest_frame = self.frames_queue.get()
+      return oldest_frame
 
-         #Matrix operations Here
+   def add_new_frame(self, frame):
+      self.frames_queue.put(frame)
 
-        return probability_matrix
+   def compute_heat_matrix(self, new_frame):
 
-   def compute_vector(self, probability_matrix)
+      #Compute heat matrix by by removing data from the last frame (divide)
+      #and add new frame (multiply)
+      heat_matrix = (self.heat_matrix / self.get_oldest_frame()) * new_frame
 
-      return [0]
+      #Add newest frame into our queue for the future
+      self.add_new_frame(new_frame)
+   
+      return heat_matrix
+
+   def compute_vector(self, new_frame):
+      
+      #Compute Newest heat matrix
+      heat_matrix = self.compute_heat_matrix(new_frame)
+      
+      #Get co-ordinate of highest value in heat_matrix
+      highest_cord = np.unravel_index(heat_matrix.argmax(), heat_matrix.shape)
+      
+      #calculate the vector as (x1-x2),(y1-y2)
+      vector = [self.shape/2,self.shape/2] - highest_cord
+      return vector
