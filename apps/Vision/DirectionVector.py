38fe145eaac54bd6b0cfe29c4b27521021adaefd
Derek <derek@w134-87-162-166.wireless.uvic.ca>
2020-01-23 19:51:45 -0800

    Hashing out functionality

--- apps/Vision/DirectionVector.py
+++ apps/Vision/DirectionVector.py
@@ -1,18 +1,33 @@
+import numpy as np
+
 class DirectionVector:
 
    def __init__(self):
-      self.frames = []
-      self.probability_matrix = []
+      self.frames_queue = queue.Queue(5)
+      self.shape = 2
+      self.heat_matrix = np.ones(shape,shape)
       pass
 
    #Take in most recent frame and return the probability matrix
-   def compute_matrix(self, frame)
-         #Add frame to frames object
+   def compute_heat_matrix(self, new_frame, frames_queue)
+         #Take oldest frame out of the queue
+         oldest_frame = frames_queue.get()
 
-         #Matrix operations Here
+         #Compute heat matrix by by removing data from the last frame (divide)
+         #and add new frame (multiply)
+         heat_matrix = (heat_matrix / oldest_frame) * new_frame
 
-        return probability_matrix
+         #Add newest frame into our queue for the future
+         frames_queue.put(new_frame)
+   
+        return heat_matrix
 
-   def compute_vector(self, probability_matrix)
-
-      return [0]
+   #only public method
+   def compute_vector(self, frame)
+      #Compute Newest heat matrix
+      heat_matrix = compute_heat_matrix(frame, frames_queue)
+      #Get co-ordinate of highest value in heat_matrix
+      highest_cord = np.unravel_index(heat_matrix.argmax(), heat_matrix.shape)
+      #calculate the vector as (x1-x2),(y1-y2)
+      vector = [shape/2,shape/2] - highest_cord
+      return vector
