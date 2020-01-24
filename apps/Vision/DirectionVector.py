6eba5c7dce4273858d367c442cdc1e7f8951af53
DerekSGov <Derekgsiemens@gmail.com>
2020-01-23 17:37:10 -0800

    Setting functions for computing matrix and final vector

--- apps/Vision/DirectionVector.py
+++ apps/Vision/DirectionVector.py
@@ -1,4 +1,18 @@
 class DirectionVector:
 
-     def __init__(self):
-        pass
+   def __init__(self):
+      self.frames = []
+      self.probability_matrix = []
+      pass
+
+   #Take in most recent frame and return the probability matrix
+   def compute_matrix(self, frame)
+         #Add frame to frames object
+
+         #Matrix operations Here
+
+        return probability_matrix
+
+   def compute_vector(self, probability_matrix)
+
+      return [0]
