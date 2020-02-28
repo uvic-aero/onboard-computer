03141b1301198c8dd421d81fe5f9cda6f2875ebd
avesk <averykushner@gmail.com>
2020-02-27 19:25:30 -0800

    adds more tests to training

--- test/test_qrcode_training.py
+++ test/test_qrcode_training.py
@@ -7,11 +7,16 @@ from apps.Vision.QRClassification.QRCodeClassification import QRCodeTraining
 
 class TestQRCodeTraining(unittest.TestCase):
 
-    def test_():
+    def test_get_training_subimages():
         """
-
+        assert that the correct number of subimages are returned
         """
         pass
+
+    def test_prep_training_data():
+        """
+        Assert that the correct number of features and samples returned
+        """
 
 
 if __name__ == '__main__':
