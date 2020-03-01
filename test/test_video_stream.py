f4982dd37fdcf0a3d29b397f343e2c2d1387b4dc
Logan Kuzyk <logan@kuzyk.net>
2020-02-29 21:06:41 -0800

    Add basic tests

--- test/test_video_stream.py
+++ test/test_video_stream.py
@@ -1,8 +1,17 @@
 import unittest
-import apps.VideoStream
+import socket
+from apps.VideoStream.videoStream import videoStream
 
 class TestVideoStream(unittest.TestCase):
 
-    def test_server(self):
+    def __init__(self):
+        self.stream = VideoStream()
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.address = ('0.0.0.0', self.stream.port) 
 
-        self.assertEqual()
+    def test_server_start(self):
+        self.assertEqual(self.stream.status, "running")
+
+    def test_make_connection(self):
+        sent = self.sock.sendto("get".encode('utf-8'), self.address)
+        self.assertEqual(len(self.stream.connections.connections), 1)
