6c60bab8e8d842ce10470b25b7eab6e4727c783b
Logan Kuzyk <logan@kuzyk.net>
2020-03-01 10:54:34 -0800

    Move service install to main install script

--- RPi-Full-Setup.sh
+++ RPi-Full-Setup.sh
@@ -6,13 +6,24 @@ sudo apt upgrade -y
 sudo apt install python3 -y
 sudo apt install python3-pip -y
 sudo apt install vim -y
-echo "Done!"
 
 echo "Installing OBC Python requirements..."
 sudo pip3 install -r requirements.txt
-echo "Done!"
 
 echo "Calling OpenCV install script..."
 sudo bash OpenCV-4-RPi-Install.sh
+
+echo "Add OBC service daemon..."
+if [ "$#" -ne 1 ]; then
+    echo "Using current directory as install directory"
+    path=$(pwd)
+else
+    path=$1
+fi
+sed -i -e "s@dir@${path}@g" ./obc.service
+sudo cp obc.service /etc/systemd/system
+sudo systemctl start obc.service
+sudo systemctl enable obc.service
+sudo systemctl daemon-reload
 
 echo "Raspberry Pi setup complete."
