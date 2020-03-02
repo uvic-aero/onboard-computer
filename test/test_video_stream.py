61a6ca4f713cb0125cba71ca834c6e9334b5fe5d
Dragon Prevost <dragon@dkp.io>
2020-03-02 11:15:46 -0800

    fixed singleton usage

--- test/test_video_stream.py
+++ test/test_video_stream.py
@@ -5,37 +5,24 @@ import time
 
 class TestVideoStream(unittest.TestCase):
 
-    # def test_server_start(self):
-    #     stream = VideoStream()
-    #     stream.start()
-    #     self.assertEqual(stream.status, "running")
-    #     stream.stop()
-    #     time.sleep(2)
+    def test_server_start(self):
+        videoStream.start()
+        self.assertEqual(videoStream.status, "running")
+        time.sleep(2)
 
-    # def test_make_connection(self):
-    #     stream = VideoStream()
-    #     stream.start()
-    #     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    #     address = ('0.0.0.0', stream.port)
-    #     sent = sock.sendto("get".encode('utf-8'), address)
-    #     self.assertEqual(len(stream.connections.connections), 1)
-    #     stream.stop()
-
-    def test_a(self):
-        vs = videoStream
-        vs.start()
-        time.sleep(3)
-        vs.stop()
-        time.sleep(3)
-        self.assertEqual(1, 1)
+    def test_server_start(self):
+        videoStream.stop()
+        self.assertEqual(videoStream.status, "down")
+        time.sleep(2)
     
-    def test_b(self):
-        vs = videoStream
-        vs.start()
-        time.sleep(3)
-        vs.stop()
-        time.sleep(3)
-        self.assertEqual(1, 1)
+    def test_make_connection(self):
+        videoStream.start()
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        address = ('0.0.0.0', videoStream.port)
+        sent = sock.sendto("get".encode('utf-8'), address)
+        self.assertEqual(len(videoStream.connections.connections), 1)
+        videoStream.stop()
+        time.sleep(2)
 
 if __name__ == '__main__':
     unittest.main()
