e79b2f0367c566d919a0095cdf433c4cf49d790f
Logan Kuzyk <logan@kuzyk.net>
2020-03-02 09:05:16 -0800

    Interim tests

--- test/test_video_stream.py
+++ test/test_video_stream.py
@@ -1,17 +1,41 @@
 import unittest
 import socket
 from apps.VideoStream.videoStream import videoStream
+import time
 
 class TestVideoStream(unittest.TestCase):
 
-    def __init__(self):
-        self.stream = VideoStream()
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.address = ('0.0.0.0', self.stream.port) 
+    # def test_server_start(self):
+    #     stream = VideoStream()
+    #     stream.start()
+    #     self.assertEqual(stream.status, "running")
+    #     stream.stop()
+    #     time.sleep(2)
 
-    def test_server_start(self):
-        self.assertEqual(self.stream.status, "running")
+    # def test_make_connection(self):
+    #     stream = VideoStream()
+    #     stream.start()
+    #     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #     address = ('0.0.0.0', stream.port)
+    #     sent = sock.sendto("get".encode('utf-8'), address)
+    #     self.assertEqual(len(stream.connections.connections), 1)
+    #     stream.stop()
 
-    def test_make_connection(self):
-        sent = self.sock.sendto("get".encode('utf-8'), self.address)
-        self.assertEqual(len(self.stream.connections.connections), 1)
+    def test_a(self):
+        vs = videoStream
+        vs.start()
+        time.sleep(3)
+        vs.stop()
+        time.sleep(3)
+        self.assertEqual(1, 1)
+    
+    def test_b(self):
+        vs = videoStream
+        vs.start()
+        time.sleep(3)
+        vs.stop()
+        time.sleep(3)
+        self.assertEqual(1, 1)
+
+if __name__ == '__main__':
+    unittest.main()
