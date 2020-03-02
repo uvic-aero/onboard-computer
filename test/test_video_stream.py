bf04617cbc32419d983dccd3d2363f7639fd7898
Logan Kuzyk <logan@kuzyk.net>
2020-03-02 13:30:09 -0800

    More tests

--- test/test_video_stream.py
+++ test/test_video_stream.py
@@ -6,25 +6,19 @@ import time
 class TestVideoStream(unittest.TestCase):
 
     def test_server_start(self):
+        self.assertEqual(videoStream.status, "down")
         videoStream.start()
         self.assertEqual(videoStream.status, "running")
         videoStream.stop()
-        time.sleep(2)
 
-    def test_server_stop(self):
-        videoStream.start()
-        videoStream.stop()
-        self.assertEqual(videoStream.status, "down")
-        time.sleep(2)
-    ''' 
     def test_make_connection(self):
         videoStream.start()
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         address = ('0.0.0.0', videoStream.port)
         sent = sock.sendto("get".encode('utf-8'), address)
         videoStream.stop()
-        time.sleep(2)
         self.assertEqual(len(videoStream.connections.connections), 1)
-    '''
+
+
 if __name__ == '__main__':
     unittest.main()
