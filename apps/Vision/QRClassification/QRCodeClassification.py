0d1e182403662d2a9a0095d99fb9d29510808548
Peter Van Gorp <p.vangorp95@gmail.com>
2020-03-05 16:59:41 -0800

    implemented functions in QRCodeClassification

--- apps/Vision/QRClassification/QRCodeClassification.py
+++ apps/Vision/QRClassification/QRCodeClassification.py
@@ -1,4 +1,10 @@
+import cv2
+import numpy as np
+import os
+
 class QRCodeClassification:
+    resolution = (1944, 2592)   # expected resolution of image. Note .shape from cv2 could be used instead
+    gridSize = 18               # common factor of image height and width, used to divide image into gridSize x Gridsize subimages
 
     def __init__(self):
 
@@ -16,9 +22,8 @@ class QRCodeClassification:
             epsilon_factor: "",
             poly_closed: ""
         }
-        pass
 
-    def exract_features(self, subimage):
+    def extract_features(self, subimage):
         """ 
         Extracts the following subimage countour features: 
             * Mean area of all contours 
@@ -31,7 +36,18 @@ class QRCodeClassification:
         Returns: 
             List: [mean_area, std_area, cardinality_of_contour]
         """
-        return [0.0, 0.0, 0.0]
+        contours = self.extract_countours(subimage)
+        areas = self.get_contour_areas(contours)
+        doc = [0.0, 0.0, 0.0]
+        if len(areas) > 0:
+            mean = np.mean(areas)
+            std = np.std(areas)
+            card = len(areas)
+            doc[0] = mean
+            doc[1] = std
+            doc[2] = float(card)
+
+        return doc
 
     def filter_contour(self, contour):
         """ 
@@ -44,9 +60,16 @@ class QRCodeClassification:
         Returns: 
             List: [mean_area, std_area, cardinality_of_contour]
         """
-        return []
+        curve_len = cv2.arcLength(contour, True)
+        approx = cv2.approxPolyDP(contour, 0.05 * curve_len, True)
+        if len(approx) >= 2 and len(approx) < 50:
+            x, y, w, h = cv2.boundingRect(approx)
+            if h > 20 and h < 1900:
+                return (x, y)
 
-    def extract_countours(self, subimage):
+        return (-1, -1)
+
+    def extract_countours(self, subimage): 
         """ 
         extracts contours from subimage
 
@@ -57,7 +80,12 @@ class QRCodeClassification:
             List of Lists of numpy arrays: Contours, 
             [[(x11,y11), ... , (x1n,y1n)], [(x21,y21), ... , (x2n,y2n)]] 
         """
-        return []
+        gray = cv2.cvtColor(subimage, cv2.COLOR_BGR2GRAY)
+        ret, thresh = cv2.threshold(gray, 120, 255, cv2.THRESH_BINARY)
+        contours, h = cv2.findContours(
+                thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        return contours
 
     def get_contour_areas(self, contours):
         """ 
@@ -70,9 +98,15 @@ class QRCodeClassification:
         Returns: 
             Float List: Areas
         """
-        return []
+        areas = []
+        for i, cont in enumerate(contours):
+            x, y = self.filter_contour(cont)
+            if x > -1:
+                areas.append(cv2.contourArea(cont))
+        
+        return areas
 
-    def split_frames(self, image):
+    def split_frames(self, image): 
         """ 
         splits image into 216x216 subimages
 
@@ -83,4 +117,14 @@ class QRCodeClassification:
         Returns: 
             List of 2D numpy arrays: Subimages
         """
-        return []
+        subimages = []
+        img = cv2.imread(image)
+        subimgHeight = self.resolution[0]//self.gridSize
+        subimgWidth = self.resolution[1]//self.gridSize
+    
+        for r in range(0, self.resolution[0],  subimgHeight):
+            for c in range(0, self.resolution[1], subimgWidth):
+                subimages.append(img[r:r+subimgHeight, c:c+subimgWidth, :])
+
+        return subimages
+
---
+++ apps/Vision/QRClassification/QRCodeClassificationTester.py
@@ -0,0 +1,17 @@
+from QRCodeClassification import QRCodeClassification
+import cv2
+import os
+
+"""
+simple test file for QRCodeClassification. takes an image as input, converts it to nparray subimages and then returns images as jpgs
+"""
+
+tmp = QRCodeClassification()
+
+subimages = tmp.split_frames("0.jpg")
+
+directory = os.fsencode("./nparray-to-image").decode()
+count = 0
+for subimage in subimages:
+    cv2.imwrite(f"./{directory}/img_{count}.jpg", subimage)
+    count += 1
