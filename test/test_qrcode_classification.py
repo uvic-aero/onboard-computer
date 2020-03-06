7d2b7044e7ef6f2465381d1c4a0254817988216c
DerekSiemens <Derekgsiemens@gmail.com>
2020-03-05 18:52:15 -0800

    Creating Unit tests

--- test/test_qrcode_classification.py
+++ test/test_qrcode_classification.py
@@ -70,7 +70,7 @@ class TestQRCodeClassification(unittest.TestCase):
         clsfn = QRCodeClassification()
 
         self.assertEquals(clsfn.filter_contour(
-            contour=contour, max_height=max_height, max_len=max_len, min_len=min_len),
+            contour=contour, min_height=min_height, max_height=max_height, max_len=max_len, min_len=min_len),
             None
         )
 
@@ -78,48 +78,185 @@ class TestQRCodeClassification(unittest.TestCase):
         """
         positive case, inbetween the two bounds
         """
+        max_height = 100
+        min_height = 2
+        max_len = 100
+        min_len = 2
+
+        # Square contour with length 4, and height 5
+        contour = np.asarray([
+            [0, 1], [1, 4],
+            [0, 0], [1, 0]
+        ])
+        clsfn = QRCodeClassification()
+
+
+        self.assertEquals(clsfn.filter_contour(
+            contour=contour, min_height=min_height, max_height=max_height, max_len=max_len, min_len=min_len),
+            contour
+        )
         pass
 
     def test_filter_countour_gt_max_ht():
         """
         negative case, exceeds upper bound
         """
+        max_height = 100
+        min_height = 2
+        max_len = 100
+        min_len = 2
+
+        # Square contour with length 4, and height 100000
+        contour = np.asarray([
+            [0, 1], [1, 100000],
+            [0, 0], [1, 0]
+        ])
+        clsfn = QRCodeClassification()
+
+
+        self.assertEquals(clsfn.filter_contour(
+            contour=contour, min_height=min_height, max_height=max_height, max_len=max_len, min_len=min_len),
+            None
+        )
         pass
 
     def test_filter_countour_vertice_lt_min():
         """
         negative case, vertices less than lower bound
         """
+        max_height = 100
+        min_height = 2
+        max_len = 100
+        min_len = 2
+
+        # Square contour with length 1, and height 1
+        contour = np.asarray([
+            [0, 1]
+        ])
+        clsfn = QRCodeClassification()
+
+        self.assertEquals(clsfn.filter_contour(
+            contour=contour, min_height=min_height, max_height=max_height, max_len=max_len, min_len=min_len),
+            None
+        )
         pass
 
     def test_filter_countour_vertice_gt_min_lt_max():
         """
         positive case, vertices is within bounds
         """
+        max_height = 100
+        min_height = 2
+        max_len = 100
+        min_len = 2
+
+        # Square contour with length 4, and height 5
+        contour = np.asarray([
+            [0, 1], [1, 4],
+            [0, 0], [1, 0]
+        ])
+        clsfn = QRCodeClassification()
+
+
+        self.assertEquals(clsfn.filter_contour(
+            contour=contour, min_height=min_height, max_height=max_height, max_len=max_len, min_len=min_len),
+            contour
+        )
         pass
 
     def test_filter_countour_vertice_gt_max():
         """
         negative case, vertices exceeds upper bound
         """
+        max_height = 100
+        min_height = 2
+        max_len = 10
+        min_len = 2
+
+        # Square contour with length 12, and height 5
+        contour = np.asarray([
+            [0, 1], [1, 4],
+            [0, 0], [1, 0],
+            [1, 1], [0, 4],
+            [1, 3], [0, 3],
+            [2, 1], [2, 4],
+            [0, 2], [2, 0]
+        ])
+        clsfn = QRCodeClassification()
+
+
+        self.assertEquals(clsfn.filter_contour(
+            contour=contour, min_height=min_height, max_height=max_height, max_len=max_len, min_len=min_len),
+            None
+        )
         pass
 
     def test_extract_contours():
         """
         assert that correct datatype is returned
         """
+        max_height = 100
+        min_height = 2
+        max_len = 100
+        min_len = 2
+
+        # Square contour with length 4, and height 5
+        contour = np.asarray([
+            [0, 1], [1, 4],
+            [0, 0], [1, 0]
+        ])
+        clsfn = QRCodeClassification()
+
+
+        self.assertEquals(type(clsfn.filter_contour(
+            contour=contour, min_height=min_height, max_height=max_height, max_len=max_len, min_len=min_len)),
+            np.ndarray
+        )
         pass
 
     def test_get_contour_areas():
         """
         assert correct datatype, irrelevant contours are filtered out
         """
+        max_height = 100
+        min_height = 2
+        max_len = 100
+        min_len = 2
+
+        # Square contour with length 4, and height 5
+        valid_contour = np.asarray([
+            [0, 1], [1, 4],
+            [0, 0], [1, 0]
+        ])
+
+        non_valid_contour = np.asarray([
+            [0, 1], [1, 1],
+            [0, 0], [1, 0]
+        ])
+        clsfn = QRCodeClassification()
+
+        #check type
+        self.assertEquals(type(clsfn.get_contour_areas(
+            contour=valid_contour, min_height=min_height, max_height=max_height, max_len=max_len, min_len=min_len)),
+            np.ndarray
+        )
+        #check invalid gets filtered out
+        self.assertEquals(clsfn.get_contour_areas(
+            contour=[valid_contour, non_valid_contour], min_height=min_height, max_height=max_height, max_len=max_len, min_len=min_len).shape[0],
+            valid_contour
+        )
         pass
 
     def test_split_frames():
         """
         assert returns correct number of subimages
         """
+        subimage = cv2.imread("test/assets/test_subimage_1.jpg")
+        clsfn = QRCodeClassification()
+        sub_images = clsfn.split_frames(subimage)
+        height, width, ____ = subimage.shape
+
+        self.assertEquals()
         pass
 
 
