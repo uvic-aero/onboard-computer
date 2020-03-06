e56a8a856e0ba1242ec6db6ceddfd4ab44dbda7b
samg9 <9gsameer@gmail.com>
2020-03-05 19:38:59 -0800

    start method in vision to kick off picam captures

--- apps/Vision/Vision.py
+++ apps/Vision/Vision.py
@@ -3,16 +3,19 @@ from apps.Vision.DropOffZoneClassifier import DropOffZoneClassifier
 from apps.Vision.LightSourceFinder import LightSourceFinder
 from apps.Vision.QRClassification.QRCodeClassifier import QRCodeClassifier
 from apps.Vision.HeatMatrix import HeatMatrix
+from apps.PiCam.piCam import piCam
 
 
 class Vision:
-
+    
     def __init__(self):
-        self.Capture = Capture()
         self.DropOffZoneClassifier = DropOffZoneClassifier()
         self.LightSourceFinder = LightSourceFinder()
         self.QRCodeClassifier = QRCodeClassifier()
         self.HeatMatrix = HeatMatrix(2,5)
 
+    def start(self):
+        while True:
+            piCam.capture() #returns numpy frame
 
 vision = Vision()
