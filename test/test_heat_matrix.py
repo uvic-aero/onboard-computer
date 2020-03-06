7a1867a0fe4d653aa9ddee3654dd0e2061b3f752
dragonprevost <dragon@dkp.io>
2020-03-05 20:27:33 -0800

    fixed resolution in heat matrix and finished tests

--- test/test_heat_matrix.py
+++ test/test_heat_matrix.py
@@ -6,33 +6,44 @@ from apps.Vision.HeatMatrix import ValueTooSmallError, InvalidOriginError
 class TestHeatMatrix(unittest.TestCase):
 
     def test_shape(self):
-        HM = HeatMatrix(2,5)
+        HM = HeatMatrix(shape=(2,2),queue_size=5)
 
         self.assertEqual(HM.shape, (2,2))
 
     def test_heat_matrix_size(self):
         with self.assertRaises(ValueTooSmallError) as cm:
-            HeatMatrix(1,2)
+            HeatMatrix(shape=(1,1),queue_size=2)
     
-    def test_heat_matrix_ones(self):
-        HM = HeatMatrix(2,5)
+    def test_heat_matrix_zeros(self):
+        HM = HeatMatrix(shape=(2,2),queue_size=5)
         self.assertTrue(
-            (HM.heat_matrix == np.ones((2,2))).all())
+            (HM.heat_matrix == np.zeros((2,2))).all())
 
     def test_queue_size(self):
-        HM = HeatMatrix(2,5)
+        HM = HeatMatrix(shape=(2,2),queue_size=5)
         
+        self.assertEqual(HM.frames_queue.qsize(), 0)
+        
+        new_frame = np.random.rand(2,2)
+        HM.compute_vector(new_frame)
+
+        self.assertEqual(HM.frames_queue.qsize(), 1)
+
+        # Check that queue does not exceed 5 frames 
+        for _ in range(10):
+           new_frame = np.random.rand(2,2)
+           HM.compute_vector(new_frame) 
+
         self.assertEqual(HM.frames_queue.qsize(), 5)
     
     def test_new_frame(self):
-        HM = HeatMatrix(2,5)
+        HM = HeatMatrix(shape=(2,2),queue_size=5)
         new_frame = np.random.rand(2,2)
-
         HM.compute_vector(new_frame)
         self.assertTrue((HM.heat_matrix == new_frame).all())
 
     def test_old_frame(self):
-        HM = HeatMatrix(2,5)
+        HM = HeatMatrix(shape=(2,2),queue_size=5)
         old_frame = np.random.rand(2,2)
 
         HM.compute_vector(old_frame)
@@ -43,16 +54,15 @@ class TestHeatMatrix(unittest.TestCase):
             (HM._get_oldest_frame() == old_frame).all())
         
     def test_origin_default(self):
-        size = 2
-        HM = HeatMatrix(size,5)
-        origin = np.array([size/2,size/2])
-
+        shape = (2,2)
+        HM = HeatMatrix(shape=shape,queue_size=5)
+        origin = np.array([shape[0]//2,shape[1]//2])
         self.assertTrue(
             (HM.origin == origin).all())
 
     def test_origin_custom(self):
         origin = np.array([0,1])
-        HM = HeatMatrix(2,5,origin=origin)
+        HM = HeatMatrix(shape=(2,2),queue_size=5,origin=origin)
 
         self.assertTrue(
             (HM.origin == origin).all())
@@ -60,10 +70,10 @@ class TestHeatMatrix(unittest.TestCase):
     def test_invalid_custom_origin(self):
         with self.assertRaises(InvalidOriginError) as cm:
             origin = np.array([2,2])
-            HeatMatrix(2,5, origin=origin)
+            HeatMatrix(shape=(2,2), queue_size=5, origin=origin)
 
     def test_compute_vector_default_origin(self):
-        HM = HeatMatrix(3,5)
+        HM = HeatMatrix(shape=(3,3),queue_size=5)
         
         new_frame = np.zeros((3,3))
         max_cord = np.array([0,2])
@@ -75,7 +85,7 @@ class TestHeatMatrix(unittest.TestCase):
 
     def test_compute_vector_custom_origin(self):
         origin = np.array([0,1])
-        HM = HeatMatrix(3,5,origin=origin)
+        HM = HeatMatrix(shape=(3,3),queue_size=5, origin=origin)
         
         new_frame = np.zeros((3,3))
         max_cord = np.array([0,2])
