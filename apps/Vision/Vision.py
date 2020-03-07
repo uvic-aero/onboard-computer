900edd943802fdebfadca64f3b5862598e634a97
avesk <averykushner@gmail.com>
2020-03-07 15:14:52 -0800

    removes capture class

--- apps/Vision/Vision.py
+++ apps/Vision/Vision.py
@@ -1,4 +1,3 @@
-from apps.Vision.Capture import Capture
 from apps.Vision.DropOffZoneClassifier import DropOffZoneClassifier
 from apps.Vision.LightSourceFinder import LightSourceFinder
 from apps.Vision.QRClassification.QRCodeClassifier import QRCodeClassifier
@@ -7,15 +6,16 @@ from apps.PiCam.piCam import piCam
 
 
 class Vision:
-    
+
     def __init__(self):
         self.DropOffZoneClassifier = DropOffZoneClassifier()
         self.LightSourceFinder = LightSourceFinder()
         self.QRCodeClassifier = QRCodeClassifier()
-        self.HeatMatrix = HeatMatrix(2,5)
+        self.HeatMatrix = HeatMatrix(2, 5)
 
     def start(self):
         while True:
-            piCam.capture() #returns numpy frame
+            piCam.capture()  # returns numpy frame
+
 
 vision = Vision()
