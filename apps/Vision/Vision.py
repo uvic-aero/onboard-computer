cbf6225bc4369c6104194f0e14fc29f9bb1581b3
avesk <averykushner@gmail.com>
2020-03-07 16:36:50 -0800

    refactor piCam

--- apps/Vision/Vision.py
+++ apps/Vision/Vision.py
@@ -3,6 +3,8 @@ from apps.Vision.LightSourceFinder import LightSourceFinder
 from apps.Vision.QRClassification.QRCodeClassifier import QRCodeClassifier
 from apps.Vision.HeatMatrix import HeatMatrix
 from apps.PiCam.piCam import piCam
+import cv2
+import time
 
 
 class Vision:
@@ -16,6 +18,13 @@ class Vision:
     def start(self):
         while True:
             piCam.capture()  # returns numpy frame
+    '''
+    #TEST WEBCAM VIDEO
+            cv2.imshow("Image", frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        cv2.destroyAllWindows()
+    '''
 
 
 vision = Vision()
