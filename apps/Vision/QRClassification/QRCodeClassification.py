6edb48a3d64057e2a67abd9f810ad8befc5f59fb
dragonprevost <dragon@dkp.io>
2020-03-12 14:13:30 -0700

    Revert "implemented functions in QRCodeClassification"
    This reverts commit e00634e24cb995235612277dfa2a10244f369a6a.

--- apps/Vision/QRClassification/QRCodeClassification.py
+++ apps/Vision/QRClassification/QRCodeClassification.py
@@ -1,10 +1,4 @@
-import cv2
-import numpy as np
-import os
-
 class QRCodeClassification:
-    resolution = (1944, 2592)   # expected resolution of image. Note .shape from cv2 could be used instead
-    gridSize = 18               # common factor of image height and width, used to divide image into gridSize x Gridsize subimages
 
     def __init__(self):
 
@@ -23,7 +17,7 @@ class QRCodeClassification:
             poly_closed: ""
         }
 
-    def extract_features(self, subimage):
+    def exract_features(self, subimage):
         """ 
         Extracts the following subimage countour features: 
             * Mean area of all contours 
@@ -36,18 +30,7 @@ class QRCodeClassification:
         Returns: 
             List: [mean_area, std_area, cardinality_of_contour]
         """
-        contours = self.extract_countours(subimage)
-        areas = self.get_contour_areas(contours)
-        doc = [0.0, 0.0, 0.0]
-        if len(areas) > 0:
-            mean = np.mean(areas)
-            std = np.std(areas)
-            card = len(areas)
-            doc[0] = mean
-            doc[1] = std
-            doc[2] = float(card)
-
-        return doc
+        return [0.0, 0.0, 0.0]
 
     def filter_contour(self, contour):
         """ 
@@ -69,7 +52,7 @@ class QRCodeClassification:
 
         return None
 
-    def extract_countours(self, subimage): 
+    def extract_countours(self, subimage):
         """ 
         extracts contours from subimage
 
@@ -80,12 +63,7 @@ class QRCodeClassification:
             List of Lists of numpy arrays: Contours, 
             [[(x11,y11), ... , (x1n,y1n)], [(x21,y21), ... , (x2n,y2n)]] 
         """
-        gray = cv2.cvtColor(subimage, cv2.COLOR_BGR2GRAY)
-        ret, thresh = cv2.threshold(gray, 120, 255, cv2.THRESH_BINARY)
-        contours, h = cv2.findContours(
-                thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-        return contours
+        return []
 
     def get_contour_areas(self, contours):
         """ 
@@ -98,15 +76,9 @@ class QRCodeClassification:
         Returns: 
             Float List: Areas
         """
-        areas = []
-        for i, cont in enumerate(contours):
-            x, y = self.filter_contour(cont)
-            if x > -1:
-                areas.append(cv2.contourArea(cont))
-        
-        return areas
+        return []
 
-    def split_frames(self, image): 
+    def split_frames(self, image):
         """ 
         splits image into 216x216 subimages
 
